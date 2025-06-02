027e56ac2ddc67aefbb75a88fc5b360bdda34e26
Cleveland6000 <153866922+Cleveland6000@users.noreply.github.com>
2025-06-02 22:17:23 +0900

    Update Vector3d.cs

--- Vector3d.cs
+++ Vector3d.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace OsmiumEngine {
 
@@ -10,14 +11,32 @@ namespace OsmiumEngine {
 
         public Vector3d (double x, double y, double z) { this.x = x; this.y = y; this.z = z; }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3d operator + (in Vector3d a, in Vector3d b) { return new Vector3d(a.x + b.x, a.y + b.y, a.z + b.z); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3d operator - (in Vector3d a, in Vector3d b) { return new Vector3d(a.x - b.x, a.y - b.y, a.z - b.z); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3d operator - (in Vector3d a) { return new Vector3d(-a.x, -a.y, -a.z); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3d operator * (in Vector3d a, in double b) { return new Vector3d(a.x * b, a.y * b, a.z * b); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3d operator * (in double b, in Vector3d a) { return new Vector3d(a.x * b, a.y * b, a.z * b); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3d operator / (in Vector3d a, in double b) { return new Vector3d(a.x / b, a.y / b, a.z / b); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator == (in Vector3d a, in Vector3d b) { return a.Equals(b); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator != (in Vector3d a, in Vector3d b) { return !(a == b); }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override readonly bool Equals (object obj) { return obj is Vector3d v && Equals(v); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override readonly int GetHashCode () { return HashCode.Combine(x, y, z); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override readonly string ToString () { return $"({x}, {y}, {z})"; }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public readonly bool Equals (in Vector3d other) { return x == other.x && y == other.y && z == other.z; }
 
         public static readonly Vector3d Zero = new Vector3d(0.0, 0.0, 0.0);
         public static readonly Vector3d UnitX = new Vector3d(1.0, 0.0, 0.0);
@@ -25,12 +44,6 @@ namespace OsmiumEngine {
         public static readonly Vector3d UnitZ = new Vector3d(0.0, 0.0, 1.0);
         public static readonly Vector3d PositiveInfinity = new Vector3d(double.PositiveInfinity, double.PositiveInfinity, double.PositiveInfinity);
         public static readonly Vector3d NegativeInfinity = new Vector3d(double.NegativeInfinity, double.NegativeInfinity, double.NegativeInfinity);
-
-        public override readonly bool Equals (object obj) { return obj is Vector3d v && Equals(v); }
-        public override readonly int GetHashCode () { return HashCode.Combine(x, y, z); }
-        public override readonly string ToString () { return $"({x}, {y}, {z})"; }
-
-        public readonly bool Equals (in Vector3d other) { return x == other.x && y == other.y && z == other.z; }
 
     }
 }
