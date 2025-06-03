6be2f2e1c1e4c6a392c347194ff10e71a0d39121
Cleveland6000 <153866922+Cleveland6000@users.noreply.github.com>
2025-06-03 21:55:12 +0900

    Update and rename Quaterniond.cs to Quaternion.cs

--- Numerics/Quaternion.cs
+++ Numerics/Quaternion.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace OsmiumEngine.Numerics {
 
-    public readonly struct Quaterniond : IEquatable<Quaterniond> {
+    public readonly struct Quaternion : IEquatable<Quaternion> {
 
         private const double DegreesToRadians = Math.PI / 180.0;
 
@@ -13,65 +13,65 @@ namespace OsmiumEngine.Numerics {
         public readonly double Z;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public Quaterniond (double w, double x, double y, double z) { W = w; X = x; Y = y; Z = z; }
+        public Quaternion (double w, double x, double y, double z) { W = w; X = x; Y = y; Z = z; }
 
-        public static readonly Quaterniond Identity = new Quaterniond(1.0, 0.0, 0.0, 0.0);
-        public static readonly Quaterniond I = new Quaterniond(0.0, 1.0, 0.0, 0.0);
-        public static readonly Quaterniond J = new Quaterniond(0.0, 0.0, 1.0, 0.0);
-        public static readonly Quaterniond K = new Quaterniond(0.0, 0.0, 0.0, 1.0);
+        public static readonly Quaternion Identity = new Quaternion(1.0, 0.0, 0.0, 0.0);
+        public static readonly Quaternion I = new Quaternion(0.0, 1.0, 0.0, 0.0);
+        public static readonly Quaternion J = new Quaternion(0.0, 0.0, 1.0, 0.0);
+        public static readonly Quaternion K = new Quaternion(0.0, 0.0, 0.0, 1.0);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public readonly double GetSqrNorm () { return W * W + X * X + Y * Y + Z * Z; }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public readonly double GetNorm () { return Math.Sqrt(W * W + X * X + Y * Y + Z * Z); }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public readonly Quaterniond GetConjugate () { return new Quaterniond(W, -X, -Y, -Z); }
+        public readonly Quaternion GetConjugate () { return new Quaternion(W, -X, -Y, -Z); }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public readonly Quaterniond GetInverse () {
+        public readonly Quaternion GetInverse () {
             double normSq = W * W + X * X + Y * Y + Z * Z;
             if (normSq == 0) {
-                return new Quaterniond(1.0, 0.0, 0.0, 0.0);
+                return new Quaternion(1.0, 0.0, 0.0, 0.0);
             }
             double invNormSq = 1.0 / normSq;
-            return new Quaterniond(W * invNormSq, -X * invNormSq, -Y * invNormSq, -Z * invNormSq);
+            return new Quaternion(W * invNormSq, -X * invNormSq, -Y * invNormSq, -Z * invNormSq);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public readonly Quaterniond GetNormalized () {
+        public readonly Quaternion GetNormalized () {
             double norm = Math.Sqrt(W * W + X * X + Y * Y + Z * Z);
             if (norm == 0) { 
                 return Identity;
             }
             double invNorm = 1.0 / norm;
-            return new Quaterniond(W * invNorm, X * invNorm, Y * invNorm, Z * invNorm);
+            return new Quaternion(W * invNorm, X * invNorm, Y * invNorm, Z * invNorm);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool operator == (in Quaterniond a, in Quaterniond b) {
+        public static bool operator == (in Quaternion a, in Quaternion b) {
             return a.W == b.W && a.X == b.X && a.Y == b.Y && a.Z == b.Z;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool operator != (in Quaterniond a, in Quaterniond b) {
+        public static bool operator != (in Quaternion a, in Quaternion b) {
             return a.W != b.W || a.X != b.X || a.Y != b.Y || a.Z != b.Z;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override readonly bool Equals (object obj) {
-            return obj is Quaterniond other && W == other.W && X == other.X && Y == other.Y && Z == other.Z;
+            return obj is Quaternion other && W == other.W && X == other.X && Y == other.Y && Z == other.Z;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public readonly bool Equals (Quaterniond other) {
+        public readonly bool Equals (Quaternion other) {
             return W == other.W && X == other.X && Y == other.Y && Z == other.Z;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Quaterniond operator * (in Quaterniond a, in Quaterniond b) {
-            return new Quaterniond(
+        public static Quaternion operator * (in Quaternion a, in Quaternion b) {
+            return new Quaternion(
                 a.W * b.W - a.X * b.X - a.Y * b.Y - a.Z * b.Z,
                 a.W * b.X + a.X * b.W + a.Y * b.Z - a.Z * b.Y,
                 a.W * b.Y - a.X * b.Z + a.Y * b.W + a.Z * b.X,
                 a.W * b.Z + a.X * b.Y - a.Y * b.X + a.Z * b.W);
         }
-        public static Vector3d operator * (in Quaterniond rotation, in Vector3d point) {
+        public static Vector3d operator * (in Quaternion rotation, in Vector3d point) {
             double x2 = rotation.X * 2.0;
             double y2 = rotation.Y * 2.0;
             double z2 = rotation.Z * 2.0;
@@ -89,7 +89,7 @@ namespace OsmiumEngine.Numerics {
                 (xy + wz) * point.X + (1.0 - (xx + zz)) * point.Y + (yz - wx) * point.Z,
                 (xz - wy) * point.X + (yz + wx) * point.Y + (1.0 - (xx + yy)) * point.Z);
         }
-        public static Quaterniond operator / (in Quaterniond a, in Quaterniond bDivisor) {
+        public static Quaternion operator / (in Quaternion a, in Quaternion bDivisor) {
             double normSqr = bDivisor.W * bDivisor.W + bDivisor.X * bDivisor.X + bDivisor.Y * bDivisor.Y + bDivisor.Z * bDivisor.Z;
             if (normSqr == 0) {
                 return Identity;
@@ -99,14 +99,14 @@ namespace OsmiumEngine.Numerics {
             double tempX = -bDivisor.X * invNormSqr;
             double tempY = -bDivisor.Y * invNormSqr;
             double tempZ = -bDivisor.Z * invNormSqr;
-            return new Quaterniond(
+            return new Quaternion(
                 a.W * tempW - a.X * tempX - a.Y * tempY - a.Z * tempZ,
                 a.W * tempX + a.X * tempW + a.Y * tempZ - a.Z * tempY,
                 a.W * tempY - a.X * tempZ + a.Y * tempW + a.Z * tempX,
                 a.W * tempZ + a.X * tempY - a.Y * tempX + a.Z * tempW);
         }
 
-        public static Quaterniond FromEulerZYX (double zAngleDeg, double yAngleDeg, double xAngleDeg) {
+        public static Quaternion FromEulerZYX (double zAngleDeg, double yAngleDeg, double xAngleDeg) {
             double xRad = xAngleDeg * DegreesToRadians;
             double yRad = yAngleDeg * DegreesToRadians;
             double zRad = zAngleDeg * DegreesToRadians;
@@ -120,10 +120,10 @@ namespace OsmiumEngine.Numerics {
             double x = cosZHalf * sinXHalf * cosYHalf - sinZHalf * cosXHalf * sinYHalf;
             double y = cosZHalf * cosXHalf * sinYHalf + sinZHalf * sinXHalf * cosYHalf;
             double z = sinZHalf * cosXHalf * cosYHalf - cosZHalf * sinXHalf * sinYHalf;
-            return new Quaterniond(w, x, y, z);
+            return new Quaternion(w, x, y, z);
         }
 
-        public static Quaterniond FromEulerZXY (double zAngleDeg, double xAngleDeg, double yAngleDeg) {
+        public static Quaternion FromEulerZXY (double zAngleDeg, double xAngleDeg, double yAngleDeg) {
             double xRad = xAngleDeg * DegreesToRadians;
             double yRad = yAngleDeg * DegreesToRadians;
             double zRad = zAngleDeg * DegreesToRadians;
@@ -137,7 +137,7 @@ namespace OsmiumEngine.Numerics {
             double x = cosYHalf * sinXHalf * cosZHalf - sinYHalf * cosXHalf * sinZHalf;
             double y = sinYHalf * cosXHalf * cosZHalf + cosYHalf * sinXHalf * sinZHalf;
             double z = cosYHalf * cosXHalf * sinZHalf - sinYHalf * sinXHalf * cosZHalf;
-            return new Quaterniond(w, x, y, z);
+            return new Quaternion(w, x, y, z);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
